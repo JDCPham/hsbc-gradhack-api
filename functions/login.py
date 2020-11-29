4563333540a1a2ffbb5e5b52db5d4641bdb0d4b6
--replace-all <jdcpham@outlook.com>
2020-11-29 00:22:21 +0000

    Login

--- functions/login.py
+++ functions/login.py
@@ -3,4 +3,16 @@ import utils.response as res
 from config import dynamodb
 
 def handler(event, context):
-    return res.build(200, {})
+
+    # Get User from DynamoDB.
+    database_response = dynamodb.get_item(
+        TableName="Users",
+        Key={
+            'Identifier': {
+                'S': '4280fa00-0f1a-41a3-8b64-fbea67238ea4'
+            }
+        }
+    )
+
+    print(database_response)
+    return res.build(200, database_response)
